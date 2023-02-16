a8e48dc001cc316cf417a79e70ee53ea1426062b
OpenDRG <gaowen_buct@hotmail.com>
2023-02-16 11:37:49 +0800

    add style

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import './style.css';
+
 const div_grouper=document.createElement('div');
-div_grouper.innerHTML='DRG分组器';
-const div_record=document.createElement('div');
-div_record.innerHTML='病案信息：';
+const div_input=document.createElement('div');
+div_input.innerHTML='病案信息：';
 const input=document.createElement('input');
 input.size=150;
 input.value='22139971,1.0,73.0,27006.0,,13030305.0,8.0,1.0,"G20.x03,R90.803,F41.101,I10.x00x002,N39.000,E77.801,E87.600",,BU21';
@@ -15,11 +16,23 @@ select.add(new Option('北京2022细分组','beijing_2022'));
 const btn=document.createElement('button');
 btn.innerHTML='提交';
 btn.onclick=click;
-div_record.appendChild(input);
-div_record.appendChild(select);
-div_record.appendChild(btn);
+div_input.appendChild(document.createElement('br'));
+div_input.appendChild(input);
+div_input.appendChild(document.createElement('br'));
+div_input.appendChild(select);
+div_input.appendChild(document.createElement('br'));
+div_input.appendChild(btn);
 const div_result=document.createElement('div');
-div_grouper.appendChild(div_record);
+const div_drg=document.createElement('div');
+const div_record=document.createElement('div');
+const div_msg=document.createElement('div');
+const ul=document.createElement('ul');
+div_msg.appendChild(ul);
+div_result.appendChild(div_record);
+div_result.appendChild(document.createElement('br'));
+div_result.appendChild(div_drg);
+div_result.appendChild(div_msg);
+div_grouper.appendChild(div_input);
 div_grouper.appendChild(div_result);
 document.body.appendChild(div_grouper);
 async function group(record,type){
@@ -37,7 +50,19 @@ function click(){
     let DRG=get_DRG(select.value);
     result.then(x=>{
         DRG.then(y=>{
-            div_result.innerHTML='record='+JSON.stringify(x.record)+'<br>'+x.drg+'-'+y[x.drg]+'<br>'+JSON.stringify(x.messages);
+            div_drg.innerHTML='result='+x.drg+'-'+y[x.drg];
+            div_drg.classList.add('red');
+            div_record.innerHTML='record='+JSON.stringify(x.record);
+            let first;
+            while (first=ul.firstElementChild){
+                first.remove(); 
+                first = ul.firstElementChild; 
+            };
+            x.messages.forEach(i=>{
+                let li=document.createElement('li');
+                li.innerHTML=i;
+                ul.appendChild(li);
+            });
         })
     });
 }
